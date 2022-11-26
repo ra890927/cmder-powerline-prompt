50d08c0a7aa149d3661f9876029fb81497fac0f4
ShihHeng <happy.890927410@gmail.com>
2022-11-26 22:46:01 +0800

    fix: find file in specify folder

--- include_powerline.lua
+++ include_powerline.lua
@@ -1,6 +1,6 @@
 -- inclue powerline file and execute
 local powerline_dir = clink.get_env('CMDER_ROOT')..'/config/powerline/'
-for _, lua_module in ipairs(clink.find_dirs(powerline_dir..'*.lua')) do
+for _, lua_module in ipairs(clink.find_files(powerline_dir..'*.lua')) do
     local file_name = powerline_dir..lua_module
     dofile(file_name)
 end
